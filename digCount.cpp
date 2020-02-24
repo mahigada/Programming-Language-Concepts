edf00825b660f5e8065c7a14f891e6847776154f
mahigada <51918723+mahigada@users.noreply.github.com>
2020-02-24 13:14:58 -0500

    Update digCount.cpp

--- digCount.cpp
+++ digCount.cpp
@@ -1,3 +1,15 @@
+/**
+Write a C++ program that reads in lines of text from the standard input.
+Keep a count of the number of lines that you read.
+Keep track of the length of the longest line.
+Keep count of the number of integers in the input. Remember to check for errors before counting something as an integer. For example, 17 is an integer but 17.4 is not.
+When there is no more input, print out three lines of output, as follows:
+NUMBER OF LINES: n1
+LENGTH OF LONGEST LINE: n2
+NUMBER OF INTEGERS: n3
+... where n1, n2, n3 are the counts your program kept when processing input.
+**/
+
 #include <iostream>
 #include <sstream>
 using namespace std;
